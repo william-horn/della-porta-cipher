4d46bbb87b0eaef0e5ce5268ade6f190a83ba65d
William Horn <williamjosephhorn@gmail.com>
2024-10-31 13:49:34 -0400

    assignment 9

--- assignment-9/WilliamHorn_BinaryValues.java
+++ assignment-9/WilliamHorn_BinaryValues.java
@@ -1,3 +1,19 @@
+/*
+ * Author: William J. Horn
+ * Written: 10/30/2024
+ * 
+ * Purpose: Convert between 2s comp in binary to decimal
+ * 
+ * Input wordSize -> 4
+ * Input firstBinaryNumber -> 1001
+ * 
+ * Output -> 1001 = -7
+ * 
+ * Input secondBinaryNumber -> 0110
+ * 
+ * Output -> 0110 = 6
+ * Output -> Word size accomodates sum of: -1
+ */
 
 
 import java.util.*;
@@ -12,25 +28,33 @@ public class WilliamHorn_BinaryValues {
    * @param <Scanner> input: The scanner object to prompt the user input
    * @param <String> message: The message to display before requesting the input
    */
-  public static int promptInt(Scanner input, String message) {
-    System.out.print(message);
-    return input.nextInt();
-  }
-
   public static String promptString(Scanner input, String message) {
     System.out.print(message);
     return input.nextLine();
   }
 
+  /*
+   * binaryToDec(<String> binaryString, <int> nonZero):
+   * 
+   * Convert a binary number to it's decimal equivalent, based on a
+   * provided nonZero parameter.
+   * 
+   * @param <String> binaryString: The binary string 
+   * @param <int> nonZero: The number to be counted as the non-zero digit in the binary number
+   * @returns <double> decimal: The decimal number converted from binary
+   */
   public static double binaryToDec(String binaryString, int nonZero) {
+    // the returned decimal value
     double decValue = 0;
 
+    // binary number properties
     int len = binaryString.length();
     int magnitude = len - 1;
 
     for (int i = 0; i < len; i++) {
       int digit = Character.getNumericValue(binaryString.charAt(i));
 
+      // if the selected digit is the nonZero, then count it's positional value
       if (digit == nonZero) {
         decValue += Math.pow(2, magnitude - i);
 
@@ -48,30 +72,85 @@ public class WilliamHorn_BinaryValues {
     return decValue;
   }
 
+  /*
+   * twosCompToDec(<String> binaryValue)
+   * 
+   * Convert a binary number in 2s comp notation to decimal
+   * 
+   * @param <String> binaryValue: The 2s comp binary number to convert
+   * @returns <double> decimalValue: The converted decimal value from 2s comp
+   */
   public static double twosCompToDec(String binaryValue) {
     int sigBit = Character.getNumericValue(binaryValue.charAt(0));
 
-    switch (sigBit) {
-      case 0 -> { return binaryToDec(binaryValue, 1); }
-      case 1 -> { return -binaryToDec(binaryValue, 0); }
-      default -> { System.out.println("Invalid binary string"); }
-    }
+    // if significant bit is 1, return standard conversion. if 0, apply 2s comp logic
+    if (sigBit == 0) return binaryToDec(binaryValue, 1); 
+    if (sigBit == 1) return -binaryToDec(binaryValue, 0) - 1;
+    
+    // forcefully return NaN - by this point the entered binaryValue was invaliud
+    return Math.sqrt(-1);
+  }
 
-    return -909;
+  /*
+   * sumIsInRange(<double> decimal, <int> wordSize)
+   * 
+   * Determine whether or not a given converted binary value to decimal
+   * falls within the size constraint defined by wordSize.
+   * 
+   * @param <double> decimal: The converted number
+   * @param <int> wordSize: The initial size constraint
+   * @returns <boolean> isInRange: Boolean true if it is in range, flase if not
+   */
+  public static boolean sumIsInRange(double decimal, int wordSize) {
+    return decimal >= -Math.pow(2, wordSize - 1)
+      && decimal <= Math.pow(2, wordSize - 1) - 1;
   }
 
   public static void main(String[] args) {
       try (Scanner input = new Scanner(System.in)) {
-        int binaryDigits = promptInt(input, "Enter size: ");
-        input.nextLine();
-        String binaryValue = promptString(input, "Enter binary value: ");
 
-        if (binaryValue.length() > binaryDigits) {
+        // collect word size and first binary number from the user
+        int wordSize = Integer.parseInt(promptString(input, "Enter size: "));
+        String binaryValue1 = promptString(input, "Enter binary value: ");
+
+        // converted decimal values
+        double decValue1 = 0;
+        double decValue2 = 0;
+
+        // compare binary number against word size
+        if (binaryValue1.length() > wordSize) {
+          System.out.println("Binary value cannot exceed size boundary");
+          System.exit(0);
+        }
+        
+        // convert the 2s comp binary to decimal
+        decValue1 = twosCompToDec(binaryValue1);
+
+        // display first binary number conversion
+        System.out.printf("%s = %s\n", binaryValue1, decValue1);
+
+        // collect second binary number
+        String binaryValue2 = promptString(input, "Enter second binary value: ");
+
+        // compare second binary number against word size
+        if (binaryValue2.length() > wordSize) {
           System.out.println("Binary value cannot exceed size boundary");
           System.exit(0);
         }
 
-        System.out.println(twosCompToDec(binaryValue));
+        // convert the 2s comp binary to decimal
+        decValue2 = twosCompToDec(binaryValue2);
+
+        // display second binary number conversion
+        System.out.printf("%s = %s\n", binaryValue2, decValue2);
+
+        // determine if the binary sum accomodates the word size
+        double decimalSum = decValue1 + decValue2;
+        
+        if (!sumIsInRange(decimalSum, wordSize))
+          System.out.println("Word size will not accomodate sum");
+        else
+          System.out.println("Word size accomodates sum of: " + decimalSum);
 
       } catch (Exception err) {
         err.printStackTrace();
