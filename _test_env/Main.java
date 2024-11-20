c390d346d1ece8a208efc4263db520ad27651bf7
William Horn <williamjosephhorn@gmail.com>
2024-11-20 14:42:17 -0500

    refactored and cleaned up IO update

--- _test_env/Main.java
+++ _test_env/Main.java
@@ -2,17 +2,16 @@
 
 
 public class Main {
+    public static void thing(Object h) {
+      h = 6;
+    }
   public static void main(String[] args) {
-      /*
-       * I am now writing this comment as a means to practice my improvisational skills and spelling,
-       * because I really am that bored. There is truly nothing else to be done at this time and I regret 
-       * ever taking this position
-       */
-      for (int i = 0; i < 13; i++) {
-          for (int j = 0; j < 13; j++) {
-              System.out.print((110 + (j + i)%13) + ((j == 12) ? " " : ", "));
-          }
-          System.out.println();
-      }
+
+      Object x = 1;
+      thing(x);
+      
+      
+      
+      System.out.println(x);
   }
 }
